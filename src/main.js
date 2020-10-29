2ce69ca8d8759b05b9600502ff4852754f9deb96
Rachel Buchta <rachelbuchta@yahoo.com>
2020-10-29 15:47:41 -0600

    Add query selectors, event handlers, and functions for iteration2

---
+++ src/main.js
@@ -0,0 +1,67 @@
+//querySelectors
+var saveButton = document.querySelector(".save-button")
+var titleInput = document.querySelector(".title-input")
+var bodyInput = document.querySelector(".body-input")
+var ideasGrid = document.querySelector(".ideas-grid")
+
+
+//event handlers
+saveButton.addEventListener("click", saveCard)
+
+// variables
+var cards = []
+
+//functions
+
+function saveCard(event) {
+  event.preventDefault()
+  // checkInput()
+  var newIdea = new Idea(titleInput.value, bodyInput.value)
+  cards.push(newIdea)
+  displayCards(cards)
+  titleInput.value = ""
+  bodyInput.value = ""
+}
+
+// function checkInput() {
+//   if (titleInput.value && bodyInput.value) {
+//     saveButton.disabled = false
+// }
+// }
+
+function displayCards(cards) {
+  ideasGrid.innerHTML = ""
+  for(var i = 0; i < cards.length; i ++) {
+  ideasGrid.innerHTML +=
+  `<div class="idea" id="">
+    <span class="star-bar">
+      <button class="fave-button">
+       <img class="star-icon" src="assets/star.svg" alt="star">
+       <img class="star-icon-active hidden" src="assets/star-active.svg" alt="red star">
+     </button>
+      <button class="delete-button">
+       <img class="delete-icon" src="assets/delete.svg" alt="delete ex">
+       <img class="delete-icon-active hidden" src="assets/delete-active.svg" alt="delete ex">
+     </button>
+    </span>
+    <article class="card-body">
+      <h3>${cards[i].title}</h3>
+      <p>${cards[i].body}</p>
+    </article>
+    <span class="comment-bar">
+      <button name="comment-button" class="comment-button">
+        <img class="comment-icon" src="assets/comment.svg" alt="adding plus">
+      </button>
+      <label for="comment-button">Comment</label>
+    </span>
+  </div>`
+}
+}
+
+
+//checking the input field - if there is no value then disable save button**
+// add hover to button in a lighter color if button is disabled (css)
+// - lighter color, cursor not pointer(css)
+//after you click save display a new card, taking an object of the idae class and using innerHTML to interpolate - createCard function
+// clear input fields - sets both .values to empty
+// stop the page from reloading - preventDefault
