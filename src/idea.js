f52fe445d283a0c77d1c23bed338ef797af3a9c7
Alice Ruppert <aliceruppert@gmail.com>
2020-11-01 00:42:55 -0600

    Create method to check if idea contains a search and boolean property indicater for readibility

--- src/idea.js
+++ src/idea.js
@@ -4,7 +4,7 @@ class Idea {
     this.title = title
     this.body = body
     this.star = false
-
+    this.containsSearch = false
   }
 
   saveToStorage() {
@@ -22,5 +22,22 @@ class Idea {
 
   toggleStar() {
     this.star = !this.star
+  }
+
+  checkIfContains(search) {
+    var userSearch = search.toUpperCase()
+    var searchedTitle = this.title.toUpperCase()
+    var searchedBody =  this.body.toUpperCase()
+
+    if (searchedTitle.includes(userSearch) || searchedBody.includes(userSearch)) {
+      this.containsSearch = true
+    }
+
+    /*
+    because our conditional returns a boolean,
+    and we're setting the property 'contains' to a boolean,
+    we could also write it as:
+    this.contains = searchedTitle.includes(userSearch) || searchedBody.includes(userSearch)
+    */
   }
 }
